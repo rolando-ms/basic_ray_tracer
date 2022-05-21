c34b51d6853cfc0d7c7ca1781773e00ae32cb712
rolando-ms <rolando.moralessuarez@gmail.com>
2022-05-21 16:51:23 +0200

    material class added (lambertian and metal), usage added

--- basicRayTracer/vec3.h
+++ basicRayTracer/vec3.h
@@ -47,6 +47,12 @@ class vec3{
             return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
         }
         
+        bool near_zero() const{
+            // Return true if vector is close to zero in all dimensions
+            const auto s{1e-8};
+            return ((fabs(e[0]) < s) && (fabs(e[1]) < s) && (fabs(e[2]) < s) );
+        }
+        
         inline static vec3 random(){
             return vec3(random_double(), random_double(), random_double());
         }
@@ -118,6 +124,10 @@ inline vec3 random_in_unit_sphere(){
 
 vec3 random_unit_vector(){
     return unit_vector(random_in_unit_sphere());
+}
+
+vec3 reflect(const vec3 &v, const vec3 &n){
+    return v - 2*dot(v,n)*n;
 }
 
 #endif // VEC3_H
